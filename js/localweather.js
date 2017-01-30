28367d59fd72f3f2e70fb64df63cbe9806bfa5e5
FreakishLancer <destfinis@gmail.com>
2017-01-30 17:42:37 -0500

    Testing Navigator.geolocation vs. ipinfo.io API

--- js/localweather.js
+++ js/localweather.js
@@ -9,9 +9,8 @@ if (navigator.geolocation) {
         currentWeatherAPI = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=40e3305e7c9e05d406ca2147ad14d791 `;
 
         $.getJSON(currentWeatherAPI, weatherData => {
-            let userCity = weatherData.name;
             
-            $("#test").text(weatherData.name);
+            $("#test").text(`${weatherData.name}, ${weatherData.sys.country} accuracy of ${position.coords.accuracy} meters.`);
             console.log(weatherData.name);
         });
     });
