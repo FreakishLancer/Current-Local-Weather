a0d8b42fd923a4b3aa721383574f476d0ee2a69e
FreakishLancer <destfinis@gmail.com>
2017-01-30 18:01:21 -0500

    Successfully implemented Ipinfo API

--- js/localweather.js
+++ js/localweather.js
@@ -2,17 +2,15 @@ let lat;
 let long;
 let currentWeatherAPI;
 
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(position => {
-        lat = position.coords.latitude;
-        long = position.coords.longitude;
+$(document).ready(() => {
+    $.getJSON("https://ipinfo.io", geoData => { // Ipinfo.io seems to be more accurate than Navigator.geolocation.
+        let posCoords = geoData.loc.split(",");
+        lat = posCoords[0];
+        long = posCoords[1];
         currentWeatherAPI = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=40e3305e7c9e05d406ca2147ad14d791 `;
-
-        $.getJSON(currentWeatherAPI, weatherData => {
-            
-            $("#test").text(`${weatherData.name}, ${weatherData.sys.country} accuracy of ${position.coords.accuracy} meters.`);
-            console.log(weatherData.name);
+        
+        $.getJSON(currentWeatherAPI, currentWeatherData => {
+            console.log(currentWeatherData.name);
         });
     });
-}
-
+});
