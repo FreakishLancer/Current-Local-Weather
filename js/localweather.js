4b6b44612c804ccb65bf2ffabba1de079e27e12e
FreakishLancer <destfinis@gmail.com>
2017-01-30 21:45:57 -0500

    Added basic functionality

--- js/localweather.js
+++ js/localweather.js
@@ -7,27 +7,27 @@ class User {
         this._country = userGeoData.country;
         this._postal = userGeoData.postal;
 
-        if (this._country === "US") {
-            this.isCelsius = false;
-            this.tempUnit = "imperial"
-        }
-        else {
-            this.isCelsius = true;
-            this.tempUnit = "metric"
-        }
+        this._country === "US" ? this.setToImperial() : this.setToMetric();
 
         this.currentWeatherAPI = `http://api.openweathermap.org/data/2.5/weather?lat=${this._lat}&lon=${this._long}&units=${this.tempUnit}&appid=40e3305e7c9e05d406ca2147ad14d791 `;
     }
 
     convertTemp() {
-        if (this.isCelsius) {
-            this.tempUnit = "metric";
-            this.isCelsius = false;
-        }
-        else {
-            this.tempUnit = "imperial";
-            this.isCelsius = true;
-        }
+        (this.isMetric) ? this.setToImperial() : this.setToMetric();
+    }
+
+    setToMetric() {
+        this.isMetric = true;
+        this.tempUnit = "metric";
+        this.tempCode = "C";
+        this.speedUnit = "meter(s)/sec";
+    }
+
+    setToImperial() {
+        this.isMetric = false;
+        this.tempUnit = "imperial";
+        this.tempCode = "F";
+        this.speedUnit = "mile(s)/hour";
     }
 }
 
@@ -35,8 +35,26 @@ $(document).ready(() => {
 
     $.getJSON("https://ipinfo.io", geoData => {
         let user = new User(geoData);
+
+        $("#user-location").text(`${user._city}, ${user._region}, ${user._country} ${user._postal}`);
+
+
+
         $.getJSON(user.currentWeatherAPI, currentWeatherData => {
-            console.log(user.currentWeatherAPI);
+            let currentTemp = currentWeatherData.main.temp;
+            let weatherParam = currentWeatherData.weather[0].main;
+            let weatherDesc = currentWeatherData.weather[0].description;
+            let weatherIcon = currentWeatherData.weather[0].icon;
+
+            console.log(weatherDesc);
+
+            $("#temperature").text(`${currentTemp} ${user.tempCode}`);
+            $("#weather-description").text(`${weatherDesc}`);
+            $("#weather-icon").attr("src", `https://openweathermap.org/img/w/${weatherIcon}.png`);
+
+            $("#temperature").on("click", () => {
+                
+            });
         });
     });
 });
