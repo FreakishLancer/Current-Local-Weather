0ecdf9762639cc1ebd05ad32b5afad26ca4b807c
FreakishLancer <destfinis@gmail.com>
2017-01-30 19:41:32 -0500

    Accounted for imperial/metric intitiation.

--- js/localweather.js
+++ js/localweather.js
@@ -1,23 +1,42 @@
 class User {
     constructor(userGeoData) { // Accepts the JSON from IPInfo API.
-        this.lat = userGeoData.loc.split(",")[0];
-        this.long = userGeoData.loc.split(",")[1];
+        this._lat = userGeoData.loc.split(",")[0];
+        this._long = userGeoData.loc.split(",")[1];
+        this._city = userGeoData.city;
+        this._region = userGeoData.region;
+        this._country = userGeoData.country;
+        this._postal = userGeoData.postal;
 
-        this.city = userGeoData.city;
-        this.region = userGeoData.region;
-        this.country = userGeoData.country;
-        this.postal = userGeoData.postal;
-        
-        this.currentWeatherAPI = `http://api.openweathermap.org/data/2.5/weather?lat=${this.lat}&lon=${this.long}&appid=40e3305e7c9e05d406ca2147ad14d791 `;
+        if (this._country === "US") {
+            this.isCelsius = false;
+            this.tempUnit = "imperial"
+        }
+        else {
+            this.isCelsius = true;
+            this.tempUnit = "metric"
+        }
+
+        this.currentWeatherAPI = `http://api.openweathermap.org/data/2.5/weather?lat=${this._lat}&lon=${this._long}&units=${this.tempUnit}&appid=40e3305e7c9e05d406ca2147ad14d791 `;
+    }
+
+    convertTemp() {
+        if (this.isCelsius) {
+            this.tempUnit = "metric";
+            this.isCelsius = false;
+        }
+        else {
+            this.tempUnit = "imperial";
+            this.isCelsius = true;
+        }
     }
 }
 
 $(document).ready(() => {
 
-    $.getJSON("https://ipinfo.io", geoData => { // Ipinfo.io seems to be more accurate than Navigator.geolocation.
-        const user = new User(geoData);
+    $.getJSON("https://ipinfo.io", geoData => {
+        let user = new User(geoData);
         $.getJSON(user.currentWeatherAPI, currentWeatherData => {
-            
+            console.log(user.currentWeatherAPI);
         });
     });
 });
